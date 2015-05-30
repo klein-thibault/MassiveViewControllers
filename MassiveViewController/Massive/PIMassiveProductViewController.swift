d9785ae1d2088a90d71c37102544abed3db9503c
Thibault Klein <thibault@prolificinteractive.com>
2015-05-29 23:13:33 -0400

    Added files for both PDPs

---
+++ MassiveViewController/Massive/PIMassiveProductViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PIMassiveProductViewController.swift
+//  MassiveViewController
+//
+//  Created by Thibault Klein on 5/29/15.
+//  Copyright (c) 2015 Prolific Interactive. All rights reserved.
+//
+
+import UIKit
+
+class PIMassiveProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
